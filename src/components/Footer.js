3549594fc91676a048b81df1c4b8b6bb6dcf66cf
dniz <dnizfor@gmail.com>
2021-11-16 02:46:29 +0300

    edit link design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,14 @@ export default function Footer() {
     return (
         <div>
             <footer className='sticky-bottom d-flex justify-content-center align-items-center '>
+                <a className="btn"  target="_blank" rel="noreferrer" href='https://github.com/dnizfor'>
                 Resources For English
-                is  open source.
-                Made with <ion-icon name="heart-outline" className='bg-danger'></ion-icon>by<a className="nav-link"  target="_blank" rel="noreferrer" href='https://github.com/dnizfor'>@dnizfor</a>
+                is  open source. 
+                Made with <ion-icon name="heart-outline" className='bg-danger'></ion-icon> by @dnizfor
+
+
+                </a>
+               
             </footer>
         </div>
     )
--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
 
 export default function HomeHeader() {
+
+    useEffect(() => {
+
+        const script = document.createElement("script");
+        script.src = "https://platform.twitter.com/widgets.js";
+        script.setAttribute("charset", "utf-8")
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+        
+    }, [])
+
     return (
         <div>
 
@@ -13,18 +27,18 @@ export default function HomeHeader() {
                         <div className="mt-5 py-2">
 
                             <div>This is a list of free resources about learning english.</div>
-                            <div>You can contribute to the project adding more resources</div>
+                            <div>You can contribute to the project adding more resources.</div>
 
                             <div className="my-2 "><a className="btn text-primary" href="https://github.com/dnizfor/resources-for-english">This project is open source.</a></div>
 
                             <div >
-                            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Learn Free English ! Most common words , series ,free books and more www.resourcesforenglish.github.com" data-show-count="false" data-size="large">Tweet</a>
+                                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Learn Free English ! Most common words , series ,free books and more www.resourcesforenglish.github.com" data-show-count="false" data-size="large">Tweet</a>
                             </div>
                         </div>
 
 
                     </div>
-                  
+
                 </div>
             </div>
 
